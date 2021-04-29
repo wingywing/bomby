0f60930b2d445f3f9c9b37a085ce72f1cca0ee58
wingywing <wingy.wing@icloud.com>
2021-04-30 00:01:39 +1000

    darkmode

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-// Toggles whether the buttons are visible or not under the header
+//Toggles whether the buttons are visible or not under the header
 collapse = document.getElementById("togglestamps-button");
 collapse.addEventListener("click", collapseButtons);
 
@@ -29,3 +29,43 @@ function changeButtonSize() {
 
 
 // //Darkmode
+document.getElementById('themeSwitch').addEventListener('change', function(event){
+  (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+});
+
+var themeSwitch = document.getElementById('themeSwitch');
+if(themeSwitch) {
+  initTheme(); // on page load, if user has already selected a specific theme -> apply it
+
+  themeSwitch.addEventListener('change', function(event){
+    resetTheme(); // update color theme
+  });
+
+  function initTheme() {
+    var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark');
+    // update checkbox
+    themeSwitch.checked = darkThemeSelected;
+    // update body data-theme attribute
+    darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+  };
+
+  function resetTheme() {
+    if(themeSwitch.checked) { // dark theme has been selected
+      document.body.setAttribute('data-theme', 'dark');
+      localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
+    } else {
+      document.body.removeAttribute('data-theme');
+      localStorage.removeItem('themeSwitch'); // reset theme selection 
+    } 
+  };
+}
+
+// Check to see if Media-Queries are supported
+if (window.matchMedia) {
+  // Check if the dark-mode Media-Query matches
+  if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    document.body.setAttribute('data-theme', 'dark');
+  } else {
+    document.body.removeAttribute('data-theme');
+  }
+}
